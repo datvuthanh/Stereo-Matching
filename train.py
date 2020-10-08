ff0b7715a1217c97a47db63068425643491e2b8e
Dat Vu <44630913+datvuthanh@users.noreply.github.com>
2020-10-08 12:19:33 +0700

    Update train.py

--- train.py
+++ train.py
@@ -8,51 +8,14 @@ from tensorflow.keras import optimizers
 from models.model import base_model
 from tensorflow.keras.models import Model
 
-def create_model(left_input,right_input):
-
-  left_model = base_model(left_input)
-  right_model = base_model(right_input) 
-
-  #print("Weight from: ",left_model.get_weights()[0])
-  # Feature extractor last layer of model
-  # prod = tf.reduce_sum(tf.multiply(left_model.output, right_model.output), axis=3, name='map_inner_product') # Batch x 1 x 201
-
-  # flatten = tf.keras.layers.Flatten()
-  # prod_flatten = flatten(prod)
-  # # Final model
-  # final_model = Model(inputs=[left_model.input, right_model.input],outputs=prod_flatten)
-  return left_model,right_model
-
 def map_inner_product(lmap, rmap):
-  #prod = tf.reduce_sum(tf.multiply(lmap, rmap), axis=3, name='map_inner_product') # Batch x 1 x 201
-  #print(prod)
   lbranch2 = tf.squeeze(lmap, [1])
   rbranch2 = tf.transpose(tf.squeeze(rmap, [1]), perm=[0, 2, 1])
   prod = tf.matmul(lbranch2, rbranch2)
-  #print(prod)
   flatten = tf.keras.layers.Flatten()
   prod_flatten = flatten(prod)
   return prod_flatten
 
-def call(left_input, right_input, training=None, mask=None, inference=False):
-    left_feature = left_model(left_input,training=training)
-    right_feature = right_model(right_input,training=training)
-    inner_product = map_inner_product(left_feature,right_feature)
-
-    return inner_product
-
-def loss_fn(left_patches,right_patches, patch_targets,training=None):
-    """Loss function."""
-    inner_product = call(left_patches,right_patches,training=training)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=patch_targets, logits=inner_product), name='loss')
-    return loss
-
-def grads_fn(left_patches,right_patches, patch_targets, training=None):
-    """Compute loss and gradients."""
-    
-    with tf.GradientTape() as tape:
-        loss = loss_fn(left_patches,right_patches, patch_targets, training)
-    return tape.gradient(loss, left_model.trainable_variables+right_model.trainable_variables), loss
 
 if __name__ == '__main__':
   flags = tf.compat.v1.app.flags
@@ -98,18 +61,18 @@ if __name__ == '__main__':
   left_input = (FLAGS.patch_size,FLAGS.patch_size,num_channels)
   right_input = (FLAGS.patch_size,FLAGS.patch_size + FLAGS.disp_range - 1, num_channels)
   
-  # Create Finally model
-  left_model,right_model = create_model(left_input,right_input)
-  
+  # Create model  
+  model = base_model((None,None,3))
+
   # Plot model
-  #plot_model(final_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-  #final_model.summary()
+  #plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+  #model.summary()
 
 
   # Create optimizer and checkpoint
   learning_rate = 0.01
   optimizer = optimizers.Adam(learning_rate)
-  ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=[left_model,right_model])
+  ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)
   manager = tf.train.CheckpointManager(ckpt, FLAGS.model_dir, max_to_keep=3)
 
   ckpt.restore(manager.latest_checkpoint)
@@ -121,17 +84,27 @@ if __name__ == '__main__':
   loss_history = []
   for _ in range(FLAGS.num_iter):
     lpatch, rpatch, patch_targets = dhandler.next_batch()  
-    grads, t_loss = grads_fn(lpatch,rpatch,patch_targets,training=True)
-    loss_history.append(t_loss.numpy().mean())
-    optimizer.apply_gradients(zip(grads, left_model.trainable_variables))
-    # After use optimizer for left model, we set weight of right model = left model
-    right_model.set_weights(left_model.get_weights())
+    #Feature extractor from model
+    with tf.GradientTape() as tape:
+
+      left_feature = model(lpatch,training=True)
+      right_feature = model(rpatch,training=True)
+      
+      # Inner product
+      inner_product = map_inner_product(left_feature,right_feature)
+      # Loss
+      loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=inner_product,labels=patch_targets), name='loss')
+    #Gradient descent
+    grads = tape.gradient(loss,model.trainable_variables)
+    loss_history.append(loss.numpy().mean())
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    
     # Add global step += 1
     ckpt.step.assign_add(1)
     if int(ckpt.step) % 100 == 0:
       save_path = manager.save()
       print("Saved checkpoint for step {}: {}".format(int(ckpt.step), save_path))
-      print('Loss at step: %d: %.6f' % (int(ckpt.step), t_loss))
+      print('Loss at step: %d: %.6f' % (int(ckpt.step), loss))
     
     if int(ckpt.step) == 24000:
       learning_rate = learning_rate / 5.
